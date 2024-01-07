6ae0a2823ff13c581af47e98b1ffdae8d75d2ea1
Saeed Dayani <101660980+TalionSD@users.noreply.github.com>
2024-01-07 16:07:18 +0330

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# audis_home_page
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/audis_home_page/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.audis_home_page
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
